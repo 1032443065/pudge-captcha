7772ff6e547666d8464b1b17d67305c7c0e71463
rozbo <75200306@qq.com>
2017-04-11 16:10:19 +0800

    启用验证码独立配置

--- src/CaptchaController.php
+++ src/CaptchaController.php
@@ -17,7 +17,7 @@ class CaptchaController
 {
     public function index($id = "")
     {
-        $captcha = new Captcha((array)Config::get('captcha'));
+        $captcha = new Captcha((array)Config::pull('captcha'));
         return $captcha->entry($id);
     }
 }
